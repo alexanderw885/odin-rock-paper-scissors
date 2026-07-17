ada29a51c1ccd6d4b22916adba9cbcb954c64cd1
Alexander Williams <alexanderw885@gmail.com>
2026-07-16 20:53:43 -0700

    Add playRound fuction and testing button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("hello world");
 
 
 
-/* 
+/**
 PLAN:
 generate random number 0, 1, or 2
 if 0, return "rock"
@@ -15,7 +15,7 @@ function getComputerChoice() {
     return output
 }
 
-/*
+/**
 PLAN:
 print "rock, paper, or scissors?"
 record input
@@ -30,9 +30,46 @@ function getHumanChoice() {
 }
 
 
+/**
+ * PLAN:
+ * run getComputerChoice, save output
+ * run getPlayerChoice, save output
+ * for each possible outcome, increment correct score
+ *  check equality for ties
+ *  check 3 player-winning cases
+ *  use else case to catch all player-losing cases
+ */
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    if (computerChoice === humanChoice){
+        console.log(`Tie! You both guessed ${computerChoice}`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors"
+            || humanChoice === "paper" && computerChoice === "rock"
+            || humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 
 let button = document.querySelector(".testComp");
 button.addEventListener("click", ()=>{console.log(getComputerChoice())});
 
 button = document.querySelector(".testGuess");
 button.addEventListener("click", ()=>{console.log(getHumanChoice())});
+
+button = document.querySelector(".testRound");
+button.addEventListener("click", () => {
+    playRound(); 
+    console.log(humanScore, computerScore)
+})
